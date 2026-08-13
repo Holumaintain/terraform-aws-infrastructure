a62f292abc6ebcd75d2fadaaabd38d810f74a1e3
Holumaintain <oluclement8@gmail.com>
2026-08-13 01:31:46 +0100

    Fix S3 bucket name variable

--- s3.tf
+++ s3.tf
@@ -3,7 +3,7 @@
 ############################################
 
 resource "aws_s3_bucket" "app_bucket" {
-  bucket = "terraform-demo-clement-20260810-148598146690"
+  bucket = var.bucket_name
 
   tags = {
     Name        = "Terraform-S3-Bucket"
